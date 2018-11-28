ea5f417285d50f4e19e2a84b6ef1c38d65a53e01
Angelitorl <an1997991@gmail.com>
2018-11-28 12:55:40 +0100

    añadido el panel
    falta que funcionen bien las colisiones

---
+++ practicaPersonajes/Assets/_Scripts/showPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showPanel : MonoBehaviour {
+    
+    public Comerciante comerciante;
+
+    // Use this for initialization
+    void Start () {
+        gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (comerciante != null)
+        {
+            gameObject.transform.GetChild(2).GetComponent<UnityEngine.UI.Text>().text = comerciante.getOro().ToString();
+            gameObject.transform.GetChild(4).GetComponent<UnityEngine.UI.Text>().text = comerciante.getMadera().ToString();
+            gameObject.transform.GetChild(6).GetComponent<UnityEngine.UI.Text>().text = comerciante.getComida().ToString();
+            gameObject.transform.GetChild(8).GetComponent<UnityEngine.UI.Text>().text = comerciante.getTabaco().ToString();
+        }
+    }
+}
---
+++ practicaPersonajes/Assets/_Scripts/showToPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showToPanel : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicked! " + gameObject.name);
+            GameObject panel = GameObject.Find("/Canvas").transform.GetChild(0).gameObject;
+            if(panel.GetComponent<showPanel>().comerciante == gameObject.GetComponent<Comerciante>())
+            {
+                panel.SetActive(false);
+            }
+            else
+            {
+                panel.GetComponent<showPanel>().comerciante = gameObject.GetComponent<Comerciante>();
+                panel.SetActive(true);
+            }
+            
+        }
+    }
+}
